438b0f2b0c912d1efaf8d06ea0eef5090c269e22
Pedro L <pedrocontact22@gmail.com>
2022-11-07 19:14:39 -0300

    RestCountries fetch + Home component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,53 @@
-import { Text, VStack } from "native-base";
+import React, { useCallback, useEffect, useState } from "react";
+import { FlatList, Text, useToast, VStack } from "native-base";
+import { useFocusEffect } from "@react-navigation/native";
+import { api } from "../services/api";
+import { Loading } from "../components/Loading";
 
 export function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [list, setList] = useState([]);
+
+  const toast = useToast();
+
+  async function fetchList() {
+    try {
+      setIsLoading(true);
+
+      const response = await api.get("/all");
+      setList(response.data);
+    } catch (error) {
+      console.log(error);
+      toast.show({
+        title: "Não foi possível carregar os bolões.",
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchList();
+    }, [])
+  );
+
   return (
     <VStack flex={1}>
       <Text>Country Flags Search</Text>
+
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          data={list}
+          keyExtractor={(item) => item.name.common}
+          renderItem={({ item }) => <Text>{item.name.official}</Text>}
+          showsVerticalScrollIndicator={false}
+        />
+      )}
     </VStack>
   );
 }
